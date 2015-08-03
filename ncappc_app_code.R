3c309ddf168741b096b7cb8c19f54c66665da8ab
Luke Fostvedt <lukefostvedt@gmail.com>
2015-08-03 14:35:26 -0700

    starting a helper file for nca
    this will return the nca output

--- ncappc_app_code.R
+++ ncappc_app_code.R
@@ -1,12 +1,21 @@
-output$NCA <- ncappc(obsFile = dmultiple,  grNm = NULL, grp =NULL,
-                     flNm = NULL, flag = NULL, doseNm = "Dose", dose = NULL,
-                     concUnit = "[ng].[mL]", timeUnit = "[hr]", doseUnit = "[mg]",
-                     doseNormUnit = NULL, obsLog = "FALSE", idNmObs = "ID", timeNmObs = "Time",
-                     concNmObs = "Conc", AUCTimeRange = c(0,24), backExtrp = "TRUE",
-                     LambdaTimeRange = NULL, LambdaExclude = NULL, doseAmtNm = "Dose",
-                     adminType = "extravascular", doseType = "ss", Tau = 24, TI = NULL,
-                     method = "mixed", timeFormat = "number",  
-                     tabCol = c("AUCtau", "Cmax", "Tmax", "Cavg", "Cmin", 
-                      "Clss", "HL_Lambda_z", "Vss_obs", "Accumulation_Index", "p_Fluctuation"), 
-                     figFormat = "png",  noPlot = "TRUE",
-                     printOut = "FALSE", studyName = NULL)
+library(grid)
+
+output$NCA = renderDataTable({
+    input$NCAestimate
+
+    ncappc(obsFile = selectedData(),  grNm = "DAY", grp =NULL,
+           flNm = NULL, flag = NULL, doseNm = "Dose", dose = NULL,
+           concUnit = "[ng].[mL]", timeUnit = "[hr]", doseUnit = "[mg]",
+           doseNormUnit = NULL, obsLog = "FALSE", idNmObs = "ID", timeNmObs = "Time",
+           concNmObs = "Conc", AUCTimeRange = c(0,Inf), backExtrp = "TRUE",
+           LambdaTimeRange = NULL, LambdaExclude = NULL, doseAmtNm = "Dose",
+           adminType = "extravascular", doseType = "ns", Tau = NULL, TI = NULL,
+           method = "mixed", timeFormat = "number",  
+           tabCol = c("AUClast", "Cmax", "Tmax", "AUCINF_obs",
+                      "Vz_obs", "Cl_obs", "HL_Lambda_z"), figFormat = "png",  noPlot = "TRUE",
+           printOut = "FALSE", studyName = "test")
+    names(dat) = c("Team", "Divison", "Record", "Colley", "Score",
+                   "Colley Rank", "Score Rank")
+    ncaOutput
+}, options = list(pageLength = 10))
+
